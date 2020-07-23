fed7060fcf04660c96d15bbae05496d78a86e77f
Axel Navarro <navarroaxel@gmail.com>
2020-07-23 11:18:37 -0300

    Fix check EAP with RELEASE license

--- src/jetbrains/fetch.rs
+++ src/jetbrains/fetch.rs
@@ -2,7 +2,10 @@ use crate::jetbrains::JetBrainsRepository;
 use reqwest::Error;
 
 pub async fn fetch_jetbrains_updates() -> Result<JetBrainsRepository, Error> {
-    let res = reqwest::get("https://www.jetbrains.com/updates/updates.xml").await?.text().await?;
+    let res = reqwest::get("https://www.jetbrains.com/updates/updates.xml")
+        .await?
+        .text()
+        .await?;
     let repository: JetBrainsRepository = serde_xml_rs::from_str(&res).unwrap();
     Ok(repository)
 }
